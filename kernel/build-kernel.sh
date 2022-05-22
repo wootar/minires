6f74373dd72b5d08837246a9d5badde73b3d3045
Wooter <rick@astley.nevergonnagiveyouup>
2022-05-22 11:14:22 +0200

    Upgrade kernel to 5.18-rc7, fix restore-apk command, add guide for ethernet and wifi connection.

--- kernel/build-kernel.sh
+++ kernel/build-kernel.sh
@@ -1,4 +1,4 @@
-wget -O kernel.tar.gz https://git.kernel.org/torvalds/t/linux-5.18-rc6.tar.gz
+wget -O kernel.tar.gz https://git.kernel.org/torvalds/t/linux-5.18-rc7.tar.gz
 mkdir modules
 tar xvf kernel.tar.gz
 mv linux-* src
