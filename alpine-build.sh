7ba3d57e4c881b4bb7545031d88b0f748d65da5a
Wooter <rick@astley.nevergonnagiveyouup>
2022-06-19 10:35:32 +0200

    Switch from initrd to ramfs, fix CI, update some stuff.

--- alpine-build.sh
+++ alpine-build.sh
@@ -4,21 +4,26 @@ echo "Downloading rootfs"
 wget -O rootfs.tar.gz https://dl-cdn.alpinelinux.org/alpine/edge/releases/x86_64/alpine-minirootfs-20220328-x86_64.tar.gz
 
 mkdir rootfs
+mkdir rootfs/rootfs
+mkdir rootfs/proc
+mkdir rootfs/sys
+mkdir rootfs/dev
+mkdir rootfs/bin
 cd rootfs
-tar xvf ../rootfs.tar.gz &> /dev/null
+tar -C rootfs xvf ../rootfs.tar.gz &> /dev/null
 cd ..
 
 echo "Configuring rootfs"
-echo "nameserver 1.1.1.1" > rootfs/etc/resolv.conf
-echo "nameserver 1.0.0.1" > rootfs/etc/resolv.conf
-echo "https://dl-cdn.alpinelinux.org/alpine/edge/main" > rootfs/etc/apk/repositories
-echo "https://dl-cdn.alpinelinux.org/alpine/edge/community" >> rootfs/etc/apk/repositories
-echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> rootfs/etc/apk/repositories
+echo "nameserver 1.1.1.1" > rootfs/rootfs/etc/resolv.conf
+echo "nameserver 1.0.0.1" > rootfs/rootfs/etc/resolv.conf
+echo "https://dl-cdn.alpinelinux.org/alpine/edge/main" > rootfs/rootfs/etc/apk/repositories
+echo "https://dl-cdn.alpinelinux.org/alpine/edge/community" >> rootfs/rootfs/etc/apk/repositories
+echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> rootfs/rootfs/etc/apk/repositories
 
 echo "echo 'Bootstrapping MiniRES on $(date '+%d-%M-%Y (DD-MM-YYYY)')'
 apk update
 apk upgrade
-apk add busybox busybox-extras htop cfdisk parted util-linux nano shellcheck links dhcpcd gpm iwd kexec-tools neofetch pfetch lynx openssh-server || exit 1
+apk add busybox busybox-static busybox-extras htop cfdisk parted util-linux nano shellcheck links dhcpcd gpm iwd kexec-tools neofetch pfetch lynx openssh-server || exit 1
 apk del openrc busybox-initscripts || true
 echo 'Fixing links to the init'
 ln -s /bin/busybox /sbin/init
@@ -34,23 +39,24 @@ rm -rf /usr/share/apk
 rm -rf /var/lib/apk
 echo 'Removing bootstrap script'
 rm /bootstrap.sh
-" > rootfs/bootstrap.sh
-cd rootfs
+" > rootfs/rootfs/bootstrap.sh
+cd rootfs/rootfs
 echo "Now for the main part, generating the image!"
 chroot . /bin/ash /bootstrap.sh || exit 1
-cd ..
+cp bin/busybox.static ../bin/busybox
+cd ../..
 
 cp -rv skeleton/* rootfs/
 DATE=$(date +%y.%m.%d)
 SED="s/REPLACEME/${DATE}/g'"
 cat skeleton/etc/os-release | sed "s/REPLACEME/${DATE}/g" > rootfs/etc/os-release
-rm rootfs/bootstrap.sh
+rm rootfs/rootfs/bootstrap.sh
 cd rootfs
-cp -rv ../kernel/modules/lib/* lib
-cp -rv ../kernel/modules/lib/* lib
+cp -rv ../kernel/modules/lib/* rootfs/lib
+cp -rv ../kernel/modules/lib/* rootfs/lib
 echo "Now generating the minires initrd"
 find . -print0 | cpio --null --verbose \
  --create --format=newc | gzip \
  --best > ../out.cpio.gz; cd .. || exit 1
 echo "Done!"
-echo "You may test it by running qemu-system-x86_64 -kernel kernel/bzImage -m 512M -accel tcg"
+echo "You may test it by running qemu-system-x86_64 -kernel kernel/bzImage -initrd -m 512M -accel tcg"
--- clean.sh
+++ clean.sh
@@ -6,3 +6,4 @@ rm -rf kernel/kernel.tar.gz
 rm -rf kernel/src
 rm -rf kernel/bzImage
 rm -rf *.gz
+rm -rf rootfs
